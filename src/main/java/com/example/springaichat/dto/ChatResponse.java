1cca9c80a5bea141afba7e331d05b371914818ce
Laleeltd@g.co.zip <ap242e@gmail.com>
2026-08-25 22:47:20 +0700

    Add ChatResponse DTO

---
+++ src/main/java/com/example/springaichat/dto/ChatResponse.java
@@ -0,0 +1,13 @@
+package com.example.springaichat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponse {
+    private String status;
+    private String message;
+}
